baf5a59260f2073a2028e684982384eff0554e0b
Shijar <shijar@sparksupport.com>
2024-03-19 00:47:09 +0530

    vulnarability report

--- code_review/analysis_code.py
+++ code_review/analysis_code.py
@@ -2,45 +2,28 @@ import subprocess
 from io import StringIO
 from pylint.lint import Run
 from pylint.reporters.text import TextReporter
-
-import pathlib
-import textwrap
-
 import google.generativeai as genai
-
-from IPython.display import display
-from IPython.display import Markdown
-# from google.colab import userdata
-
-# GOOGLE_API_KEY=userdata.get('AIzaSyCPH0cf7oSdP28TFhTqgxobAnOmr3QrV1A')
-
 genai.configure(api_key='AIzaSyCPH0cf7oSdP28TFhTqgxobAnOmr3QrV1A')
-
-def to_markdown(text):
-  text = text.replace('•', '  *')
-  return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
-
 
 def analyze_code(code):
             
     pylint_output = StringIO()  # Custom open stream
     reporter = TextReporter(pylint_output)
-    Run([f'media/{code}', "--disable=import-error,invalid-name"], reporter=reporter, exit=False)
+    Run([f'media/{code}', "--disable=import-error,invalid-name", "--msg-template='{path}:{line}:{column}: {msg_id}: {msg} ({symbol})'"], reporter=reporter, exit=False)
     result = pylint_output.getvalue().replace("\n", "<br>").replace((f'media\{code}').replace('/', '\\'), '').replace('*', '')
     lines = result.split('<br>')
     lines.pop(0)
     result_text = '<br>'.join(lines)
-    print(result_text)
+
+    with open (f'media/{code}', 'r') as c:
+        l = c.read()
     model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content("Give me some linter report for this code" + codeto)
-    with open('result_text.py', 'w') as f:
-        f.write(response.text.replace('```python', '').replace('```', ''))
-    return result_text
+    response = model.generate_content("Give me a vulnarability report of this code for displaying in a html page using innerHTMl " + l)
+
+    return {'linter': result_text, 'vulnerability': response.text.replace("\n", "<br>")}
 
 def fix_code(code):
     check = subprocess.check_output(f"python -m black --diff --verbose media/{code}")
     path = "media/"+code
     fix = subprocess.run(["python", "-m", "black", "--verbose", path], capture_output=True)
-    # return fix.decode().replace('\r\n', '<br>')
-    print(fix, 'dadda')
     return fix.stderr
